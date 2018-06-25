aaf0951662259fff222436ce816a4d22c968290a
Ronaldo <kikogamer@gmail.com>
2018-06-24 21:13:18 -0300

    Criando service raffle + Preparação de Testes

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,7 +7,10 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
-# Add additional requires below this line. Rails is not loaded until this point!
+
+RSpec.configure do |config|
+  config.include Devise::Test::ControllerHelpers, type: :controller
+end
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
