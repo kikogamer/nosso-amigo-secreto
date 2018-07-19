8b56b571a5851f5386a571f8dda616403855efef
Ronaldo <kikogamer@gmail.com>
2018-07-18 22:37:16 -0300

    ajustando controller home para redirecionar para as campanhas caso o usuário já tenha ao menos uma criada

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,11 @@
 class PagesController < ApplicationController
   def home
+    if user_signed_in?
+      campaigns = current_user.campaigns
+
+      if (campaigns.count > 0)
+        redirect_to "/campaigns"
+      end
+    end
   end
 end
